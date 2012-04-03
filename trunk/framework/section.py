3350da7d439b52155cfb4863fe083d029bf3bc98
imransomji@gmail.com <imransomji@gmail.com@e59d3f1e-f1c4-fbfc-c205-892edba9ba68>
2012-04-03 22:28:54 +0000

    Using memcache for hierarchy

--- trunk/framework/section.py
+++ trunk/framework/section.py
@@ -18,6 +18,7 @@ Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 """
 
 from google.appengine.ext import db
+from google.appengine.api import memcache
 from google.appengine.api import users
 
 from framework.subsystems import template
@@ -51,7 +52,7 @@ class Section(db.Model):
             'login_url': users.create_login_url(loginout_url),
             'self': self,
             'classes': 'section' + self.path.replace('/', '-').rstrip('-'),
-            'primary_ancestor': self.get_primary_ancestor(),
+            'primary_ancestor': get_primary_ancestor(self.path),
             'main': self.content() if self.path_parts[2] else '<h2>Under Construction</h2>Main content goes here',
         }
         return template.html(params)
@@ -67,12 +68,6 @@ class Section(db.Model):
         if not permission.perform_action(content, self.path_parts):
             raise Exception('Permission denied', self.path_parts[0], self.path_parts[1])
         return content
-    
-    def get_primary_ancestor(self):
-        ancestor = self
-        while ancestor.parent_path:
-            ancestor = get_section(self.handler, [ancestor.parent_path])
-        return ancestor
 
 def section_key(path):
     return db.Key.from_path('Section', path)
@@ -90,14 +85,43 @@ def get_section(handler, path_parts):
             return section
         raise Exception('Page not found', path_parts)
 
+def get_primary_ancestor_helper(path, hierarchy):
+    for p in hierarchy:
+        if path == p or get_primary_ancestor_helper(path, hierarchy[p][1]):
+            return hierarchy[p]
+    return None
+
+def get_primary_ancestor(path):
+    return get_primary_ancestor_helper(path, cache_get_full_hierarchy())
+
 def get_first_level(path):
-    return Section.gql("WHERE parent_path IN :1", ['', None])
+    hierarchy = cache_get_full_hierarchy()
+    sections = []
+    for k in hierarchy:
+        sections.append(hierarchy[k])
+    return sections
 
 def get_second_level(path):
-    section = get_section(None, [path])
-    while section.parent_path:
-        section = get_section(None, [section.parent_path])
-    return Section.gql("WHERE parent_path=:1", section.path)
+    top = get_primary_ancestor(path)
+    children = []
+    for k in top[1]:
+        children.append(top[1][k])
+    return children
+
+def db_get_hierarchy(path=None):
+    ret = {}
+    for s in Section.gql("WHERE parent_path=:1", path):
+        ret[s.path] = {'path': s.path, 'parent_path': s.parent_path, 'title': s.title, 'name': s.name, 'keywords': s.keywords, 'description': s.description, 'rank': s.rank, 'is_private': s.is_private}, db_get_hierarchy(s.path)
+    return ret
+
+def cache_get_full_hierarchy():
+    hierarchy = memcache.Client().get('section_hierarchy')
+    if hierarchy is not None:
+        return hierarchy
+    else:
+        hierarchy = db_get_hierarchy()
+        memcache.Client().set('section_hierarchy', hierarchy)
+        return hierarchy
 
 def is_ancestor(path, another_path):
     while path != another_path:
@@ -110,7 +134,7 @@ def is_ancestor(path, another_path):
 def can_path_exist(path, parent_path, old_path=None):
     if not path:
         raise Exception('Path is required')
-    elif is_ancestor(path, parent_path):
+    elif parent_path and is_ancestor(path, parent_path):
         raise Exception('Path recursion detected: Path is a descendant')
     elif is_ancestor(parent_path, path):
         raise Exception('Path recursion detected: Path is an ancestor')
@@ -129,27 +153,31 @@ def can_path_exist(path, parent_path, old_path=None):
     return True
 
 def create_section(handler, path, parent_path=None, name='', title='', force=False):
+    path = path.replace('/', '').replace(' ', '').strip().lower() if path else None
+    parent_path = parent_path.replace('/', '').replace(' ', '').strip().lower() if parent_path else None
     if not force and not can_path_exist(path, parent_path): return None
     section = Section(parent=section_key(path), path=path.lower(), parent_path=parent_path.lower() if parent_path else None, name=name, title=title)
     section.put()
     section.handler = handler
     section.path_parts = [path, parent_path, None, None]
+    memcache.Client().delete('section_hierarchy')
     return section
 
 def update_section(old, path, parent_path, name, title):
-    path = path.replace('/', '').replace(' ', '').strip().lower()
-    parent_path = parent_path.replace('/', '').replace(' ', '').strip().lower()
+    path = path.replace('/', '').replace(' ', '').strip().lower() if path else None
+    parent_path = parent_path.replace('/', '').replace(' ', '').strip().lower() if parent_path else None
     if old.path != path:
         can_path_exist(path, parent_path, old.path)
         for child in Section.gql("WHERE parent_path=:1", old.path):
             child.parent_path = path
             child.put()
-        new = Section(parent=section_key(path), path=path.lower(), parent_path=parent_path.lower(), name=name, title=title)
+        new = Section(parent=section_key(path), path=path, parent_path=parent_path, name=name, title=title)
         old.delete()
         new.put()
     elif old.parent_path != parent_path:
         can_path_exist(path, parent_path, old.path)
-    old.parent_path = parent_path
+    old.parent_path = parent_path # TODO: This seems wrong, check it
     old.name = name
     old.title = title
     old.put()
+    memcache.Client().delete('section_hierarchy')
