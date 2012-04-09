7e0b3af04abf58581194a9521a093d9836c9eeeb
imransomji@gmail.com <imransomji@gmail.com@e59d3f1e-f1c4-fbfc-c205-892edba9ba68>
2012-04-09 15:35:59 +0000

    - Manage navigation style changes - Restructured view filter into view_global, setting it up to create view_local

--- trunk/framework/content/base/__init__.py
+++ trunk/framework/content/base/__init__.py
@@ -21,25 +21,30 @@ Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 from google.appengine.ext import db
 
 class base:
-    
+
     path = db.StringProperty()
     rank = db.IntegerProperty()
     section_path = db.StringProperty()
-    
-    base_permissions = {
-                        'administrate': 'Administer Permissions'
-    }
-    content_permissions = {}
-    
-    def permissions(self):
-        self.base_permissions.update(self.content_permissions)
-        return self.base_permissions
-    
+
+    base_actions = {'administrate': 'Administer permissions'}
+    content_actions = {}
+
+    base_views = {}
+    content_views = {}
+
+    def all_actions(self):
+        self.base_actions.update(self.content_actions)
+        return self.base_actions
+
+    def all_views(self):
+        self.base_views.update(self.content_views)
+        return self.base_views
+
     def __init__(self, section, handler, path_parts):
         self.section = section
         self.handler = handler
         self.path_parts = path_parts
-        
+
     def __str__(self):
         # If the action doesn't exist, the AttributeError will lead to a 404
         return getattr(self, 'action_%s' % self.path_parts[2])()
