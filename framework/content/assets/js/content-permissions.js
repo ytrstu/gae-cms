d38897c80497bc5a4ac7309d8fa918abd4f221f1
imransomji <imransomji@e59d3f1e-f1c4-fbfc-c205-892edba9ba68>
2012-04-17 03:45:04 +0000

    Prevent flicker for permissions menu

--- framework/content/assets/js/content-permissions.js
+++ framework/content/assets/js/content-permissions.js
@@ -2,9 +2,11 @@ YUI().use("node-menunav", function(Y) {
 
 	Y.all('.content-permissions').each(function(i) {
 
-	    Y.on("contentready", function () {
+	    Y.on('contentready', function () {
 
 	        this.plug(Y.Plugin.NodeMenuNav, { autoSubmenuDisplay: false, mouseOutHideDelay: 0 });
+
+	    	this.all('.yui3-menu').setStyle('visibility','visible');
 
 	    }, "#" + i.get('id'));
 
